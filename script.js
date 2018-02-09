2d3f3342654e215a447b20677170db5bae89a190
Maurice de Jong <20231852+mauricejuuhh@users.noreply.github.com>
2018-02-09 11:31:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let kaders = document.querySelectorAll('.deel');
+const svgns = "http://www.w3.org/2000/svg";
+
+const maakSVGs = () => {
+for (let i = 0; i < kaders.length; i++) {
+    //bepaal de breedte
+  let breedte = kaders[i].offsetWidth;
+  let hoogte = kaders[i].offsetHeight;
+  let omtrek = (breedte+hoogte)*2;
+
+  let fig = document.createElementNS(svgns, 'svg');
+  let rh = document.createElementNS(svgns, 'rect');
+
+  let stijlString = 'stroke-dasharray:'+omtrek+'; stroke-dashoffset: ' + omtrek +';';
+  let stijlStringHover = 'stroke-dasharray:'+omtrek+'; stroke-dashoffset: 0;';
+  rh.setAttribute('width', breedte);
+  rh.setAttribute('height', hoogte);
+  rh.setAttribute('class', 'deel__rechthoek');
+  rh.setAttribute('style', stijlString);
+
+  rh.addEventListener("mouseover", () => {
+    rh.setAttribute('style', stijlStringHover);
+  });
+
+  rh.addEventListener("mouseout", () => {
+    rh.setAttribute('style', stijlString);
+  });
+  fig.setAttribute('width', breedte);
+  fig.setAttribute('height', hoogte);
+  fig.setAttribute('class', 'deel__kader');
+  fig.appendChild(rh);
+
+  kaders[i].appendChild(fig);
+
+}
+}
+const verwijderSVGs = () => {
+  let alleSVGs = document.querySelectorAll('.deel__kader');
+  while (alleSVGs[0]) {
+    alleSVGs[0].parentNode.removeChild(alleSVGs[0]);
+    alleSVGs = document.querySelectorAll('.deel__kader');
+  }
+}
+maakSVGs();
+window.addEventListener('resize', () => {
+  verwijderSVGs();
+  maakSVGs();
+});
